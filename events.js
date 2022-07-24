3633bcf69cdf04b49be6607fdbd7bdf717236882
Chidobe <thedobecompany@gmail.com>
2022-07-24 20:26:32 +0100

    iterating through nodelist and adding events listeners

--- events.js
+++ events.js
@@ -2,4 +2,13 @@ const btn = document.querySelector("#btn");
 btn.onclick = () => alert("hello world");
 
 const botn = document.querySelector('#botn');
-botn.addEventListener('click',() => {alert("hello world")});
+botn.addEventListener('click', function(e){
+    e.target.style.background = 'blue';
+});
+
+// const butts = document.querySelectorAll('button');
+//  butts.forEach((press) => {
+//     press.addEventListener('click', () => {
+//         alert(press.id)
+//     });
+//  });
