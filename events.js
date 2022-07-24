0fbc3f153ab488d5b0963f6277d305ce9c96f36b
Chidobe <thedobecompany@gmail.com>
2022-07-24 08:33:56 +0100

    method 3

--- events.js
+++ events.js
@@ -1,3 +1,5 @@
 const btn = document.querySelector("#btn");
 btn.onclick = () => alert("hello world");
 
+const botn = document.querySelector('#botn');
+botn.addEventListener('click',() => {alert("hello world")});
